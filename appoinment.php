0d95a74ecd3b0037dc0077405cbb600f1bc45401
Dharmjit Singh <85618900+Dharmjit-san@users.noreply.github.com>
2022-05-24 14:51:10 +0530

    Update appoinment.php

--- appoinment.php
+++ appoinment.php
@@ -771,7 +771,7 @@ if(isset($_GET['status'])){
     <div class="main-box clearfix">
   <div id="tabs">
   <ul>
-    <li><a href="#tabs-1">All</a></li>
+    <li><a href="#tabs-1">Driver</a></li>
     <li><a href="#tabs-2">Appointments</a></li> 
  
     <li><a href="#tabs-3">No Show</a></li> 	
@@ -1058,7 +1058,7 @@ window.llcustomer=function(xnj){
 } 
  setInterval(function(){
  
-    myajax( {"api":"getcustomersdetails","type":type },function( data, textStatus, jQxhr ){ 
+    myajax( {"api":"getcustomersdetails","type":type,"adminCountryCode":"<?php echo $_SESSION['COUNTRYCODE']; ?>"},function( data, textStatus, jQxhr ){ 
          $(".rowaxcustomer").remove();
          var _clone;
          var tbd=$("#tbdcustomers"); 
